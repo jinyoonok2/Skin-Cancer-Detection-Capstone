8b7d5756f28d545099a34574773e1e121c9b309b
Jinyoon Kim <84653106+jinyoonok2@users.noreply.github.com>
2024-01-19 11:08:32 -0500

    update

--- dataset_preprocess/detr_coco_json/coco_dataset_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-COCO_DATASET_DIR = r'C:\Jinyoon Projects\datasets\combined_dataset_coco'
-COCO_YAML_PATH = os.path.join(COCO_DATASET_DIR, 'data.yaml')
-COCO_OLD_YAML_PATH = os.path.join(COCO_DATASET_DIR, 'data_old_class_10.yaml')
-
-COCO_TRAIN_PATH = os.path.join(COCO_DATASET_DIR, 'train')
-COCO_VALID_PATH = os.path.join(COCO_DATASET_DIR, 'valid')
-COCO_TEST_PATH = os.path.join(COCO_DATASET_DIR, 'test')
--- dataset_preprocess/yolo_txt/dataset_path.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DATASET_DIR = r'C:\Jinyoon Projects\datasets\combined_dataset' # YOLO dataset
-DEFAULT_PATH = os.path.join(DATASET_DIR, 'default')
-YAML_PATH = os.path.join(DATASET_DIR, 'data.yaml')
-OLD_YAML_PATH = os.path.join(DATASET_DIR, 'data_old_class_10.yaml')
-TRAIN_PATH = os.path.join(DATASET_DIR, 'train')
